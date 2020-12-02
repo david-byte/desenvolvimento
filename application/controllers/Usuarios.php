64f061e0a3c6a6180a0d8034b0c1c2f16c013592
David Anderson Developer <davidanderson04@live.com>
2020-12-02 19:22:37 -0300

    Fazendo uma validação para nao carregar css e js desnecessários

---
+++ application/controllers/Usuarios.php
@@ -0,0 +1,30 @@
+<?php 
+
+defined('BASEPATH') or exit('Ação não permitida');
+
+class Usuarios extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data = array(
+            'title' => 'Usuarios',
+            'styles' => array(
+                'vendor/datatables/dataTables.bootstrap4.min.css'
+            ),
+            'scripts' => array(
+                'vendor/datatables/jquery.dataTables.min.js',
+                'vendor/datatables/dataTables.bootstrap4.min.js',
+                'vendor/datatables/app.js'
+            ),
+        );
+
+        $this->load->view('layout/header',$data);
+        $this->load->view('usuarios/index');
+        $this->load->view('layout/footer');
+    }
+}
