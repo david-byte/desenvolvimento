34333962b6ebc714a75eb069f19a470318a663ce
David Anderson Developer <davidanderson04@live.com>
2020-12-05 19:47:41 -0300

    configuraçao da tela de login

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,13 +1,10 @@
-    <?php $this->load->view('layout/header_login'); ?>
-    <div class="container">
+<div class="container">
         <!-- Outer Row -->
-        <div class="row justify-content-center margin-top-5">
-
+        <div class="row justify-content-center">
             <div class="col-xl-10 col-lg-12 col-md-9">
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
-
                         <div class="row">
                             <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
                             <div class="col-lg-6">
@@ -23,21 +20,31 @@
                                         </div>
                                     </div>
                                 <?php endif; ?>
+                                <?php if ($menssage = $this->session->flashdata('info')) : ?>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="alert alert-info alert-dismissible fade show" role="alert">
+                                                <strong><i class="fas fa-exclamation-triangle">&nbsp;&nbsp;</i><?php echo $menssage; ?></strong>
+                                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Bem Vindo ao&nbsp;<?php echo $h1; ?></h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Seja bem vindo!</h1>
                                     </div>
                                     <form class="user" name="form_auth" method="POST" action="<?php echo base_url('login/auth'); ?>">
                                         <div class="form-group">
-                                            <input type="email" name="email" class="form-control form-control-user"placeholder="Digite seu e-mail...">
+                                            <input type="email" name="email" class="form-control form-control-user" placeholder="Entre com seu E-mail...">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" name="password" class="form-control form-control-user" placeholder="Digite sua senha...">
+                                            <input type="password" name="password" class="form-control form-control-user" placeholder="Senha">
                                         </div>
-
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            Entrar
-                                        </button>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">Entrar</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
